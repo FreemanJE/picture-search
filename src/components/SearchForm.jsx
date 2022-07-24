31fd4e92bce7f602089acfcf10fbbe590179ad71
Freeman <pfreejohnson@gmail.com>
2022-07-24 21:12:52 +0200

    Title Updated

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -15,13 +15,7 @@ const SearchForm = () => {
     
     useEffect(() => {
         inputRef.current.focus()
-        // inputRef.current.style.backgroundColor = 'skyblue'
-        // inputRef.current.style.height = '40px'
-        // inputRef.current.style.textAlign = 'center'
-        // inputRef.current.style.width = '40%'
-        // inputRef.current.style.boxShadow = "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset";
-        // inputRef.current.style.borderRadius = "10px"
-        // inputRef.current.style.fontSize = "20px"
+        
         
       }, [])      
 
